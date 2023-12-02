586d4c321c2d79b0203f2ee8683c20f860dbd0c2
YokochiO <rx422337@gk2.so-net.ne.jp>
2023-12-02 15:48:22 +0900

    Astroアプデとか

--- src/scripts/getcats.ts
+++ src/scripts/getcats.ts
@@ -1,4 +1,4 @@
 const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=9')
-const cats: [{ url: string; width: number; height: number }] = await res.json()
+const cats: { url: string; width: number; height: number }[] = await res.json()
 
 export default cats
